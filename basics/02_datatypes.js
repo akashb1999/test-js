05331b2629a00089542458c27a75a05cf215fb04
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2025-07-05 18:48:18 +0000

    new lesson

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"//treat all js codes as new js codes versions
+//alert( 3 + 3)
+// //we're using node js , not browser!!
+
+let name = "Akash"
+let age = 26
+let isLoggedIn = true
+
+
+//number => 2 powered by 53
+//bigint
+//string => ""/''
+//Booliun => True/False
+//null => Standalone value (Representation of a Zero)
+//undifined => nothing entered
+//Symbol => Unique something
+
+//object...
+
+console.table(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
+
