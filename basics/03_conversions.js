0576326eda11df6ff9bc08e1ed541c694d2e479f
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2025-07-05 20:14:03 +0000

    conversion of datatypes

---
+++ basics/03_conversions.js
@@ -0,0 +1,20 @@
+let score = 30
+let name = "Akash"
+let something = null
+console.log(typeof score);
+console.log (typeof name)
+console.log(typeof something)
+
+
+let valueInNumber = String (score)
+console.log(typeof valueInNumber);
+let changeInNumber = Number(name)
+console.log(typeof changeInNumber);
+console.log(something)
+console.log(name)
+console.log(changeInNumber)
+console.log(valueInNumber)
+//"30"=>30 
+//"Akash"=> NaN
+//true=>1; false=>0
+//null=>null/0
