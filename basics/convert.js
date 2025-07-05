a5ae46cafcff50b7d111e51bcfdf04dd885c0a2b
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2025-07-05 20:48:36 +0000

    calculations in convertions

---
+++ basics/convert.js
@@ -0,0 +1,22 @@
+let value = 2
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Akash"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log(1 + 1)
+// console.log(1 - 1)
+// console.log(1 * 1)
+// console.log(1 ** 1)
+// console.log(1 / 1)
+// console.log(1 % 1)
+//***********************************************/
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + "2")
+console.log("1" + 2 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
