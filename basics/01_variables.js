d00ee3856eb577252004d51b1430f6bea6cad1d7
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2025-07-05 18:04:53 +0000

    Variables

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "something@gmail.com"
+var accountPassword = "6789"
+accountCity = "Rampurhat"
+let accountState;
+
+//accountId = 3// not allowed
+accountEmail = "bcdf@gmail.com"
+accountPassword = "212121"
+accountCity = "Kolkata"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
