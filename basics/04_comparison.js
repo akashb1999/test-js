321ad91edb44aeb40523df34b171173253392da9
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2025-07-06 15:42:36 +0000

    type comparison

---
+++ basics/04_comparison.js
@@ -0,0 +1,22 @@
+/*
+console.log("2" > 1);//true
+console.log("02" > 1);//true
+*/
+
+/*
+console.log("akash" > 1);//false
+console.log("Bhatt" < 2);//false
+console.log("Akash" >= 2);//false
+console.log("Akash" * 2);//Nan
+console.log("Akash" / 2); //NaN
+console.log("Akash" != 1);//true
+console.log(null > 0)//false
+console.log(null < 0)//false
+console.log(undefined != 0)//true
+console.log(null != 0)//true
+console.log(undefined >= 0)//false
+console.log(undefined = 0)//0
+console.log(undefined != 0)//true
+console.log(null >= 0)//true
+*/
+console.log(typeof ("2" === 2)) //strict cheack  
