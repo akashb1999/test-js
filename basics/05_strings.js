2f8f5ffb69d266af7b8397c6e1bd34c635a34cd5
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2025-07-06 20:01:19 +0000

    strings and string functions

---
+++ basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Akash"
+const age = 26
+console.log(`Hello, my name is ${name},and my age is ${age}`)
+
+const name2 = new String ('Akash B...')
+console.log (name2)
+console.log (name2.length)
+console.log (name2. toUpperCase())
+console.log (name2. charAt('s'))
+console.log (name2. charAt(2))
+console.log (name2. indexOf('s'))
+
+let newstr = name2.substring(0, 4)
+console.log (newstr)
+let newName = "          akash bhbb           "
+console.log(newName)
+console.log (newName.trim())
+
+const url = "http://akash.com/akash%20bhatta/"
+console.log (url.replace('%20','--'))
+console.log (url.includes('kash'))
+
+console.log (url.split('a'))
